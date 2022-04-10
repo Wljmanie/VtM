de167ef360032f763ae079b9642f79725a62d00c
Wljmanie <45335409+Wljmanie@users.noreply.github.com>
2022-04-10 07:42:28 +0200

    Add project files.

---
+++ VtM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VtM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
